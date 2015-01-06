131c20e3eb2cb8a3b02a19a2a15a574cc2680f24
Adam Engebretson <adam@enge.me>
2015-01-06 17:09:04 -0600

    Adding framework integration
    Signed-off-by: Adam Engebretson <adam@enge.me>

---
+++ src/Adamgoose/AnnotationsServiceProvider.php
@@ -0,0 +1,245 @@
+<?php namespace Adamgoose;
+
+use Adamgoose\Console\EventScanCommand;
+use Adamgoose\Console\RouteScanCommand;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Support\ServiceProvider;
+use Adamgoose\Events\Annotations\Scanner as EventScanner;
+use Adamgoose\Routing\Annotations\Scanner as RouteScanner;
+
+class AnnotationsServiceProvider extends ServiceProvider {
+
+    /**
+     * The commands to be registered.
+     *
+     * @var array
+     */
+    protected $commands = [
+      'EventScan' => 'command.event.scan',
+      'RouteScan' => 'command.route.scan',
+    ];
+
+    /**
+     * The classes to scan for event annotations.
+     *
+     * @var array
+     */
+    protected $scanEvents = [];
+
+    /**
+     * The classes to scan for route annotations.
+     *
+     * @var array
+     */
+    protected $scanRoutes = [];
+
+    /**
+     * Determines if we will auto-scan in the local environment.
+     *
+     * @var bool
+     */
+    protected $scanWhenLocal = false;
+
+    /**
+     * File finder for annotations.
+     *
+     * @var AnnotationFinder
+     */
+    private $finder;
+
+    /**
+     * @param \Illuminate\Contracts\Foundation\Application $app
+     */
+    public function __construct(Application $app)
+    {
+        $this->finder = new AnnotationFinder($app);
+        parent::__construct($app);
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerCommands();
+    }
+
+    /**
+     * Register the application's annotated event listeners.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadAnnotatedEvents();
+
+        if ( ! $this->app->routesAreCached())
+        {
+            $this->loadAnnotatedRoutes();
+        }
+    }
+
+    /**
+     * Register the commands.
+     *
+     * @return void
+     */
+    protected function registerCommands()
+    {
+        foreach (array_keys($this->commands) as $command)
+        {
+            $method = "register{$command}Command";
+            call_user_func_array([$this, $method], []);
+        }
+        $this->commands(array_values($this->commands));
+    }
+
+
+    /**
+     * Register the command.
+     *
+     * @return void
+     */
+    protected function registerEventScanCommand()
+    {
+        $this->app->singleton('command.event.scan', function ($app)
+        {
+            return new EventScanCommand($app['files']);
+        });
+    }
+
+    /**
+     * Register the command.
+     *
+     * @return void
+     */
+    protected function registerRouteScanCommand()
+    {
+        $this->app->singleton('command.route.scan', function ($app)
+        {
+            return new RouteScanCommand($app['files']);
+        });
+    }
+
+    /**
+     * Load the annotated events.
+     *
+     * @return void
+     */
+    public function loadAnnotatedEvents()
+    {
+        if ($this->app->environment('local') && $this->scanWhenLocal)
+        {
+            $this->scanEvents();
+        }
+
+        if ( ! empty($this->scanEvents) && $this->finder->eventsAreScanned())
+        {
+            $this->loadScannedEvents();
+        }
+    }
+
+    /**
+     * Scan the events for the application.
+     *
+     * @return void
+     */
+    protected function scanEvents()
+    {
+        if (empty($this->scanEvents))
+        {
+            return;
+        }
+
+        $scanner = new EventScanner($this->scanEvents);
+
+        file_put_contents(
+          $this->finder->getScannedEventsPath(), '<?php ' . $scanner->getEventDefinitions()
+        );
+    }
+
+    /**
+     * Load the scanned events for the application.
+     *
+     * @return void
+     */
+    protected function loadScannedEvents()
+    {
+        $events = $this->app['Illuminate\Contracts\Events\Dispatcher'];
+
+        require $this->finder->getScannedEventsPath();
+    }
+
+    /**
+     * Load the annotated routes
+     *
+     * @return void
+     */
+    protected function loadAnnotatedRoutes()
+    {
+        if ($this->app->environment('local') && $this->scanWhenLocal)
+        {
+            $this->scanRoutes();
+        }
+
+        if ( ! empty($this->scanRoutes) && $this->finder->routesAreScanned())
+        {
+            $this->loadScannedRoutes();
+        }
+    }
+
+    /**
+     * Scan the routes and write the scanned routes file.
+     *
+     * @return void
+     */
+    protected function scanRoutes()
+    {
+        if (empty($this->scanRoutes))
+        {
+            return;
+        }
+
+        $scanner = new RouteScanner($this->scanRoutes);
+
+        file_put_contents($this->finder->getScannedRoutesPath(),
+          '<?php ' . $scanner->getRouteDefinitions());
+    }
+
+    /**
+     * Load the scanned application routes.
+     *
+     * @return void
+     */
+    protected function loadScannedRoutes()
+    {
+        $this->app->booted(function ()
+        {
+            $router = $this->app['Illuminate\Contracts\Routing\Registrar'];
+
+            require $this->finder->getScannedRoutesPath();
+        });
+    }
+
+    /**
+     * Get the classes to be scanned by the provider.
+     *
+     * @return array
+     */
+    public function eventScans()
+    {
+        return $this->scanEvents;
+    }
+
+    /**
+     * Get the classes to be scanned by the provider.
+     *
+     * @return array
+     */
+    public function routeScans()
+    {
+        return $this->scanRoutes;
+    }
+}
