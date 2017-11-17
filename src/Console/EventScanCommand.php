4679191a0f3a5d32f6d82d0103fad187e7f58835
lpcdernoncourt <32063876+lpcdernoncourt@users.noreply.github.com>
2017-11-17 18:07:26 +0100

    Update EventScanCommand.php
    (cherry picked from commit 29eafb2)

--- src/Console/EventScanCommand.php
+++ src/Console/EventScanCommand.php
@@ -57,7 +57,7 @@ class EventScanCommand extends Command
      *
      * @return void
      */
-    public function fire()
+    public function handle()
     {
         $this->files->put($this->getOutputPath(), $this->getEventDefinitions());
 
