bf4bded7071b484c51323e0ee2553fccebf40e37
Alex Luneburg <loonytoons003@gmail.com>
2019-04-12 09:53:30 +1200

    Replace tabs with spaces

--- tests/Routing/results/route-detail-any.php
+++ tests/Routing/results/route-detail-any.php
@@ -1,13 +1,13 @@
 <?php
 
 return [
-	[
-		'verb' => 'any',
-		'path' => 'my-any-route',
-		'uses' => 'App\\Http\\Controllers\\AnyController@anyAnnotations',
-		'as' => null,
-		'middleware' => [],
-		'where' => [],
-		'domain' => null,
-	],
+    [
+        'verb' => 'any',
+        'path' => 'my-any-route',
+        'uses' => 'App\\Http\\Controllers\\AnyController@anyAnnotations',
+        'as' => null,
+        'middleware' => [],
+        'where' => [],
+        'domain' => null,
+    ],
 ];
