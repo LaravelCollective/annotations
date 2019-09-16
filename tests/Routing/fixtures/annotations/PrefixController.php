09dd4813e132f44d643c87b1ec4de565e760a7a8
Somoza <julian.somoza@gmail.com>
2019-09-16 13:06:38 -0300

    Added unit testing

---
+++ tests/Routing/fixtures/annotations/PrefixController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+/**
+ * @Controller(prefix="/any-prefix")
+ */
+
+
+class PrefixController
+{
+    /**
+     * @Get("route-with-prefix", as="route.with.prefix")
+    */
+    public function withPrefixAnnotations()
+    {
+    }
+
+    /**
+     * @Get("route-without-prefix", as="route.without.prefix", no_prefix="true")
+    */
+    public function withoutPrefixAnnotations()
+    {
+    }
+}
