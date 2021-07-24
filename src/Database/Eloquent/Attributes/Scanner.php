d4ba159ed0cb711783db65320a3705454b85313d
ggg-mzkr <mzkr.ggg@gmail.com>
2021-07-25 03:46:50 +0900

    feat: Implement Attribute for Database

---
+++ src/Database/Eloquent/Attributes/Scanner.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Collective\Annotations\Database\Eloquent\Attributes;
+
+use Collective\Annotations\AnnotationScanner;
+use Collective\Annotations\Database\Eloquent\Annotations\InvalidBindingResolverException;
+use Collective\Annotations\Database\Eloquent\Attributes\Attributes\Bind;
+use ReflectionAttribute;
+use ReflectionClass;
+
+class Scanner extends AnnotationScanner
+{
+    /**
+     * Convert the scanned annotations into route definitions.
+     *
+     * @throws InvalidBindingResolverException
+     *
+     * @return string
+     */
+    public function getModelDefinitions()
+    {
+        $output = '';
+
+        /** @var ReflectionClass $class */
+        foreach ($this->getClassesToScan() as $class) {
+            $attributes = $class->getAttributes();
+
+            if (count($attributes) > 0 && !$this->extendsEloquent($class)) {
+                throw new InvalidBindingResolverException('Class ['.$class->name.'] is not a subclass of [Illuminate\Database\Eloquent\Model].');
+            }
+
+            foreach ($attributes as $attribute) {
+                $output .= $this->buildBinding($attribute->newInstance()->binding, $class->name);
+            }
+        }
+
+        return trim($output);
+    }
+
+    /**
+     * Determine if a class extends Eloquent.
+     *
+     * @param ReflectionClass $class
+     *
+     * @return bool
+     */
+    protected function extendsEloquent(ReflectionClass $class)
+    {
+        return $class->isSubclassOf('Illuminate\Database\Eloquent\Model');
+    }
+
+    /**
+     * Build the event listener for the class and method.
+     *
+     * @param string $binding
+     * @param string $class
+     *
+     * @return string
+     */
+    protected function buildBinding($binding, $class)
+    {
+        return sprintf('$router->model(\'%s\', \'%s\');', $binding, $class).PHP_EOL;
+    }
+}
